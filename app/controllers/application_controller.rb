dc9a9a71db0ded3bb5459b0b102e8f2bbfcb1083
zxbuzzy <zxbuzzy@gmail.com>
2021-11-15 22:48:17 +0300

    return the current time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  # GET
+  def index
+    time = {
+      "date": Time.now.strftime("%d/%m/%Y"),
+      "time": Time.now.strftime("%k:%M")
+    }
+    render json: time.to_json
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module PrototypeScheduleApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.api_only = true
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -35,6 +35,5 @@ module PrototypeScheduleApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
   end
 end
